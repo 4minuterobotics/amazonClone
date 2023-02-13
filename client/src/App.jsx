1f8c7193c20146591586ec1b32fb5b59f7ec7a36
William Lawrence <lawrence.william86@gmail.com>
2023-02-12 19:23:27 -0500

    listed products by creating products array, adding product images, rendering products, and styling them

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import data from './data';
+
+const App = () => {
+	return (
+		<div>
+			<header>
+				<a href='/'>Amazon Clone</a>
+			</header>
+			<main>
+				<h1>Featured Products</h1>
+				<div className='products'>
+					{data.products.map((product) => (
+						<div className='product' key={product.slug}>
+							<a href={`/product/${product.slug}`}>
+								<img src={product.image} alt={product.name} />
+							</a>
+							<div className='product-info'>
+								<a href={`/product/${product.slug}`}>
+									<p>{product.name}</p>
+								</a>
+								<p>
+									<strong>${product.price}</strong>
+								</p>
+								<button>Add to cart</button>
+							</div>
+						</div>
+					))}
+				</div>
+			</main>
+		</div>
+	);
+};
+
+export default App;
---
+++ client/src/data.js
@@ -0,0 +1,55 @@
+//this file will hold an object to return products
+const data = {
+	products: [
+		{
+			name: 'Nike Slim Shirt',
+			slug: 'nike-slim-shirt',
+			category: 'Shirts',
+			image: '/images/p1.jpeg', //all images are 679px x 829px
+			price: 120,
+			countInStock: 10,
+			brand: 'Nike',
+			rating: 4.5,
+			numReviews: 10,
+			description: 'high quality shirt',
+		},
+		{
+			name: 'Addidas Fit Shirt',
+			slug: 'addidas-fit-shirt',
+			category: 'Shirts',
+			image: '/images/p2.jpeg',
+			price: 150,
+			countInStock: 13,
+			brand: 'Addidas',
+			rating: 3.5,
+			numReviews: 9,
+			description: 'high quality product',
+		},
+		{
+			name: 'Nike Slim Pants',
+			slug: 'nike-slim-pants',
+			category: 'Pants',
+			image: '/images/p3.jpeg',
+			price: 230,
+			countInStock: 10,
+			brand: 'Nike',
+			rating: 4.8,
+			numReviews: 15,
+			description: 'high quality product',
+		},
+		{
+			name: 'Addidas Fit Pants',
+			slug: 'addidas-fit-pants',
+			category: 'Pants',
+			image: '/images/p4.jpeg',
+			price: 249,
+			countInStock: 9,
+			brand: 'Addidas',
+			rating: 4.8,
+			numReviews: 18,
+			description: 'high quality pants',
+		},
+	],
+};
+
+export default data;
