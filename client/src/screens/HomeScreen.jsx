e6cabe3f3e6c9da19ec10d4e6c89e03c5dc1a909
4minuterobotics <lawrence.william86@gmail.com>
2023-04-14 15:17:28 -0400

    removed reduce logger dependency

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useReducer, useState } from 'react';
-import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
@@ -24,7 +23,7 @@ const reducer = (state, action) => {
 };
 
 const HomeScreen = () => {
-	const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+	const [{ loading, error, products }, dispatch] = useReducer (reducer, {
 		loading: true,
 		error: '',
 		products: [],
